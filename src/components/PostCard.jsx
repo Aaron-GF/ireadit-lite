ecf3ff607189ff1b14c39e9bce66aa17247392f2
Aaron-GF <aarongarcia567@gmail.com>
2025-09-24 16:13:13 +0200

    fix: change subredit by default for deploy on vercel.

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,5 +1,3 @@
-"use client"
-
 export default function PostCard({ posts }) {
   const p = posts;
 
--- src/lib/features/posts/postsSlice.js
+++ src/lib/features/posts/postsSlice.js
@@ -4,7 +4,7 @@ const postsSlice = createSlice({
   name: "posts",
   initialState: [],
   reducers: {
-    setPosts: (state, action) => action.payload,
+    setPosts: (_, action) => action.payload,
   },
 });
 
