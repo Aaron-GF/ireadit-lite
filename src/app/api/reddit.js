b63e5706cb24ec408b132ca39ca0bfcd98c865bd
Aaron-GF <aarongarcia567@gmail.com>
2025-09-24 16:13:13 +0200

    fix: change subredit by default for deploy on vercel.

--- src/app/api/reddit.js
+++ src/app/api/reddit.js
@@ -1,5 +1,5 @@
 /* call initial posts */
-export async function getInitialPosts(subreddit = "popular") {
+export async function getInitialPosts(subreddit = "javascript") {
   const res = await fetch(
     `https://www.reddit.com/r/${subreddit}.json?raw_json=1`
   );
